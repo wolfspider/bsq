afbb18d1622f43d67708e55028be47943f24daa8
wolfspider <jesse.alexander.bennnett@gmail.com>
2021-07-17 14:51:57 -0400

    update paths to be relative

--- build.rs
+++ build.rs
@@ -6,12 +6,12 @@ fn main() {
     
     //let out_dir = PathBuf::from(env::var("OUT_DIR")
         //.expect("OUT_DIR env var is not defined"));
-    let include_path = "/usr/home/basilisk/tapedeck/bsq/src";
-    let include_path2 = "/usr/home/basilisk/tapedeck/bsq/src/bsqcustom";
+    let include_path = "src";
+    let include_path2 = "src/bsqcustom";
     let lib_path = "/usr/local/lib/";
     let flag = "-std=c++17";
     //let lflag = "-lc++"; 
-    cpp_build::Config::new().flag(flag).include(include_path).include(include_path2).build("/usr/home/basilisk/tapedeck/bsq/src/lib.rs");
+    cpp_build::Config::new().flag(flag).include(include_path).include(include_path2).build("src/lib.rs");
     println!("cargo:rustc-link-search={}", lib_path);
     //println!("cargo:rustc-link-lib=myexternallib");
 }
